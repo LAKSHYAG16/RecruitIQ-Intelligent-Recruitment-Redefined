53e78890b6dc4bb31a6c9fa59d3ca75c52a94d84
LAKSHYAG16 <lgarg1609@gmail.com>
2025-02-23 01:00:17 +0530

    check

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,9 @@
 import NextAuth from 'next-auth'
-// import AppleProvider from 'next-auth/providers/apple'
-// import FacebookProvider from 'next-auth/providers/facebook'
-// import GoogleProvider from 'next-auth/providers/google'
-// import EmailProvider from 'next-auth/providers/email'
+import AppleProvider from 'next-auth/providers/apple'
+import FacebookProvider from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
+import EmailProvider from 'next-auth/providers/email'
 import GitHubProvider from "next-auth/providers/github";
-
 
 export const authoptions = NextAuth({
   providers: [
@@ -35,21 +34,3 @@ export const authoptions = NextAuth({
 })
 
 export { authoptions as GET, authoptions as POST}
-
-
-
-
-
-// import NextAuth from 'next-auth';
-// import GitHubProvider from "next-auth/providers/github";
-
-// const handler = NextAuth({
-//   providers: [
-//     GitHubProvider({
-//       clientId: process.env.GITHUB_ID,
-//       clientSecret: process.env.GITHUB_SECRET,
-//     }),
-//   ],
-// });
-
-// export { handler as GET, handler as POST };
